81dfd2cc26d7b83ae949825cda53cb0a2afcedf7
Maxi Learner <sagmeister@gmail.com>
2019-03-21 21:14:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// var evens = [0, 2, 4, 6, 8, 10]
-
 function doToElementsInArray(array, callback) {
   array.forEach(callback)
 }
@@ -8,7 +6,3 @@ function doToElementsInArray(array, callback) {
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`
 }
-//
-// var animals = ["dog", "cat", "squirrel"]
-
-// changeCompletely(animals, changeCompletely)
